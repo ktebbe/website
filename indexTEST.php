8408b6bf470642847603b63f2ae061f3320566aa
Kira Tebbe <kira.tebbeibm.com@kiras-mbp.cambridge.ibm.com>
2017-10-20 16:41:29 -0400

    making test files for navbar

--- indexTEST.php
+++ indexTEST.php
@@ -12,6 +12,7 @@
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<script src="https://code.jquery.com/jquery-1.10.2.js"></script> 
 	</head>
 	<body class="homepage">
 		<div id="page-wrapper">
@@ -31,37 +32,14 @@
 							</footer>
 						</div>
 
-					<!-- Nav -->
-						<nav id="nav">
-							<ul>
-								<li><a href="index.html">Home</a></li>
-								<li>
-									<a href="#">Projects</a>
-									<ul>
-										<li>
-											<a href="#">Quantitative &rarr;</a>
-											<ul>
-												<li><a href="math-thesis.html">Thesis for Applied Math BA</a></li>
-												<li><a href="nba.html">NBA Hackathon</a></li>
-												<li><a href="#">RDS Analysis Tool</a></li>
-											</ul>
-										</li>
-										<li>
-											<a href="#">Qualitative &rarr;</a>
-											<ul>
-												<li><a href="socy-thesis.html">Thesis for Sociology BA</a></li>
-												<li><a href="schedule.html">Schedule Redesign</a></li>
-											</ul>
-										</li>
-										<li><a href="#">Veroeros feugiat</a></li>
-									</ul>
-								</li>
-								<li><a href="map.html">Map</a></li>
-								<li><a href="left-sidebar.html">Left Sidebar</a></li>
-								<li><a href="right-sidebar.html">Right Sidebar</a></li>
-								<li><a href="no-sidebar.html">No Sidebar</a></li>
-							</ul>
-						</nav>
+					<!-- Nav: read in nav.html file -->
+<!-- 					<div id="nav-placeholder"></div>
+						<script>
+						$(function(){
+						  $("#nav-placeholder").load("nav.html");
+						});
+						</script> -->
+						<?php include("nav.php");?>
 
 				</div>
 
---
+++ nav.html
@@ -0,0 +1,30 @@
+<nav id="nav">
+	<ul>
+		<li><a href="index.html">Home</a></li>
+		<li>
+			<a href="#">Projects</a>
+			<ul>
+				<li>
+					<a href="#">Quantitative &rarr;</a>
+					<ul>
+						<li><a href="math-thesis.html">Thesis for Applied Math BA</a></li>
+						<li><a href="nba.html">NBA Hackathon</a></li>
+						<li><a href="#">RDS Analysis Tool</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Qualitative &rarr;</a>
+					<ul>
+						<li><a href="socy-thesis.html">Thesis for Sociology BA</a></li>
+						<li><a href="schedule.html">Schedule Redesign</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Veroeros feugiat</a></li>
+			</ul>
+		</li>
+		<li><a href="map.html">Map</a></li>
+		<li><a href="left-sidebar.html">Left Sidebar</a></li>
+		<li><a href="right-sidebar.html">Right Sidebar</a></li>
+		<li><a href="no-sidebar.html">No Sidebar</a></li>
+	</ul>
+</nav>
